505d72c446bb09d4eea11292380ac926a3baefd1
Lucy <lucybellott@gmail.com>
2023-05-03 18:08:30 -0400

    api key

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  const apiKey = '0416392f-b1f5-4318-af0c-2586c336c1ec'
+  const apiCall = `http://api.airvisual.com/v2/city?city=Los Angeles&state=California&country=USA&key=${apiKey}`
+  
+  
   return (
     <div className="App">
       <h1>Allergy app</h1>
