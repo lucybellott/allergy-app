4f6b81ae8bbd3f931d8ec731a3e02c6ca3a6a9eb
Lucy <lucybellott@gmail.com>
2023-05-05 14:37:57 -0400

    api key

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react'
 import './App.css';
 
 function App() {
   
   const apiKey = '0416392f-b1f5-4318-af0c-2586c336c1ec'
-  const apiCall = `http://api.airvisual.com/v2/city?city=Los Angeles&state=California&country=USA&key=${apiKey}`
+  const apiCall = `http://api.airvisual.com/v2/city?city=Los%20Angeles&state=California&country=USA&key=${apiKey}`
   
   
+const [airIfo, setInfo] = useState({})
+
+ 
+  useEffect(() => {
+    fetch(`${apiCall}`)
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data)
+    })
+    }, [])
+
+
+
+    // curl --request GET \
+    //  --url 'https://api.openaq.org/v2/cities?limit=100&page=1&offset=0&sort=asc&order_by=city' \
+    //  --header 'accept: application/json'
+
+ 
   return (
     <div className="App">
-      <h1>Allergy app</h1>
+      <h1>Air app</h1>
+      <div></div>
     </div>
   );
 }
